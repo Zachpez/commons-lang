36f74439a40795d664f65c0133120fb730d08a11
Zpesarc <Zpesarc@bgsu.edu>
2025-04-25 11:49:37 -0400

    Test 4

--- src/test/java/org/apache/commons/lang3/builder/CompareToBuilderTest.java
+++ src/test/java/org/apache/commons/lang3/builder/CompareToBuilderTest.java
@@ -54,4 +54,36 @@ import org.junit.jupiter.api.Test;
         assertEquals(0, new CompareToBuilder().append(a, a).toComparison());
         assertEquals(0, new CompareToBuilder().append(b, b).toComparison());
     }
+    
+    //Tests for the append method that compares integer arrays
+    @Test
+    public void appendIntArrayTest(){
+        int[] a = {1};
+        int[] b = {10};
+        int[] c = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] d = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] e = {2, 3, 4, 5, 6, 7, 8, 8, 10};
+
+        //Self Equality
+        assertEquals(0, new CompareToBuilder().append(a, a).toComparison());
+        assertEquals(0, new CompareToBuilder().append(c, c).toComparison());
+        
+        //Null handling
+        assertTrue(new CompareToBuilder().append(null, a).toComparison() < 0);
+        assertTrue(new CompareToBuilder().append(a, null).toComparison() > 0);
+        
+        //Single Element comparison
+        assertTrue(new CompareToBuilder().append(a, b).toComparison() < 0);
+        assertTrue(new CompareToBuilder().append(b, a).toComparison() > 0);
+
+        //Array Length Difference
+        assertTrue(new CompareToBuilder().append(a, c).toComparison() < 0);
+        assertTrue(new CompareToBuilder().append(c, a).toComparison() > 0);
+        
+        //Multi Element Comparison
+        assertTrue(new CompareToBuilder().append(c, d).toComparison() < 0);
+        assertTrue(new CompareToBuilder().append(d, c).toComparison() > 0);
+        assertTrue(new CompareToBuilder().append(c, e).toComparison() < 0);
+        assertTrue(new CompareToBuilder().append(e, c).toComparison() > 0);
+    }
  }
